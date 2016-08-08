b4bdc3469d64c44313c3ae1db2b79c80e06b3178
johnharding <johnharding@fastmail.fm>
2016-08-08 11:20:07 +0100

    Post-modern Pearson Restructuring

---
+++ src/DesignSpace/DesignSpaceAttributes.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.GUI;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Grasshopper.Kernel.Attributes;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace DesignSpace
+{
+    public class DesignSpaceAttributes : GH_ComponentAttributes
+    {
+
+        public DesignSpaceComponent MyOwner
+        {
+            get;
+            private set;
+        }
+
+        public DesignSpaceAttributes(DesignSpaceComponent owner) : base(owner)
+        {
+            this.MyOwner = owner;
+        }
+
+        protected override void Layout()
+        {
+            base.Layout();
+        }
+
+
+        public override GH_ObjectResponse RespondToMouseDoubleClick(GH_Canvas sender, GH_CanvasMouseEvent e)
+        {
+            if ((ContentBox.Contains(e.CanvasLocation)))
+            {
+                //CurrentOwner.SOLVE = true;
+
+                // hit the export straight away, where we handle the folder selection! 
+                //CurrentOwner.Export();
+                //CurrentOwner.ExpireSolution(true);
+
+                //MyOwner.GO = true;
+                System.Media.SystemSounds.Beep.Play();
+
+                return GH_ObjectResponse.Handled;
+            }
+
+           
+
+            return GH_ObjectResponse.Ignore;
+        }
+
+
+
+        protected override void Render(GH_Canvas canvas, Graphics graphics, GH_CanvasChannel channel)
+        {
+            if (channel == GH_CanvasChannel.Wires)
+            {
+                base.Render(canvas, graphics, channel);
+            }
+
+            if (channel == GH_CanvasChannel.Objects)
+            {
+                // 1. Component Render
+                base.Render(canvas, graphics, channel);
+
+                // 2. Canvas Segmentation
+                //graphics.DrawLine(Pens.Black, 0, 0, 0, -100000);
+                //graphics.DrawLine(Pens.Black, 0, 0, -100000, 0);
+                //graphics.FillEllipse(Brushes.Black, -2, -2, 4, 4);
+
+                //Font ubuntuFont = new Font("ubuntu", 8);
+                //StringFormat format = new StringFormat();
+                //format.Alignment = StringAlignment.Near;
+                //format.LineAlignment = StringAlignment.Center;
+                //format.Trimming = StringTrimming.EllipsisCharacter;
+
+                //graphics.DrawString("Child", ubuntuFont, Brushes.Black, 10,-20, format);
+                //graphics.DrawString("Parent", ubuntuFont, Brushes.Black, 10, 20, format);
+                //graphics.DrawString("Ingredients", ubuntuFont, Brushes.Black, -80, 20, format);
+
+                /*
+                GH_Palette palette = GH_Palette.Pink;
+
+                Color myColor = Color.LightGray;
+
+                
+                switch (Owner.RuntimeMessageLevel)
+                {
+                    case GH_RuntimeMessageLevel.Warning:
+                        myColor = Color.Orange;
+                        break;
+
+                    case GH_RuntimeMessageLevel.Error:
+                        myColor = Color.Red;
+                        break;
+                }
+                 
+
+                if (Owner.Hidden) myColor = Color.Gray;
+                if (Owner.Locked) myColor = Color.DarkGray;
+
+                RectangleF myRect = new RectangleF(Bounds.Location, Bounds.Size);
+                GH_Capsule capsule = GH_Capsule.CreateCapsule(myRect, palette, 10, 0);
+
+                capsule.Render(graphics, myColor);
+                capsule.Dispose();
+                capsule = null;
+
+                base.RenderComponentCapsule(canvas, graphics, false, false, false, true, true, false);
+
+                
+
+                PointF iconLocation = new PointF(ContentBox.X-4, ContentBox.Y+70);
+                graphics.DrawImage(Owner.Icon_24x24, iconLocation);
+                */
+
+                //format.Dispose();
+
+            }
+        }
+
+    }
+}
---
+++ src/DesignSpace/DesignSpaceComponent.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace DesignSpace
+{
+    public class DesignSpaceComponent : GH_Component
+    {
+        public bool GO = false;
+        private List<Grasshopper.Kernel.Special.GH_NumberSlider> sliders = new List<Grasshopper.Kernel.Special.GH_NumberSlider>();
+        int counter;
+
+        public DesignSpaceComponent() 
+            : base("DesignSpace", "DS", "Generates new grasshopper networks automatically", "Extra", "Default")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pm)
+        {
+            pm.AddBooleanParameter("Go", "G", "Set to true to run through solutions", GH_ParamAccess.item, false);
+            pm.AddNumberParameter("Sliders", "S", "Put numeric variables (sliders) in here", GH_ParamAccess.list);
+
+            pm[1].WireDisplay = GH_ParamWireDisplay.faint;
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pm)
+        {
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+           
+
+            // If we are currently static, then reset things and collect sliders
+            if (!GO)
+            {
+                counter = 0;
+                sliders.Clear();
+
+                // Default value is false anyway remember
+                DA.GetData("Go", ref GO);
+                
+                foreach (IGH_Param param in this.Params.Input[1].Sources)
+                {
+                    Grasshopper.Kernel.Special.GH_NumberSlider slider = param as Grasshopper.Kernel.Special.GH_NumberSlider;
+                    if (slider != null)
+                    {
+                        sliders.Add(slider);
+                    }
+                }
+            }
+
+            else
+            {
+                // Test to see if we can change the slider to 9
+                sliders[0].Slider.Value = (decimal)counter;
+
+                // Iterate the master counter
+                counter++;
+
+                // If we reach a limit, then stop
+                if (counter == 5)
+                {
+                    GO = false;
+
+                    //expire this component
+                    this.ExpireSolution(true);
+                }
+
+            }
+
+           
+        }
+
+
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("73F1D5F1-7208-4501-8C8C-66ED25BA5A1D"); }
+        }
+
+        public override void CreateAttributes()
+        {
+            m_attributes = new DesignSpaceAttributes(this);
+        }
+
+
+        public override GH_Exposure Exposure
+        {
+            get
+            {
+                return GH_Exposure.primary;
+            }
+        }
+
+        protected override Bitmap Icon
+        {
+            get
+            {
+                return Properties.Resources.DesignSpaceIcon;
+            }
+        }
+    }
+}
